94c0244d00ba33be5aff24084727fe9c09c5f75f
Mateusz Ziomek <mziomek@andea.com>
2019-08-22 10:51:18 +0200

    Diffusion Done

--- Navier-Stokes/__main__.py
+++ Navier-Stokes/__main__.py
@@ -3,15 +3,26 @@
 # 1. 1D Linear Convection
 from steps.linearconvection1d import LinearConvection1D
 from steps.nonlinearconvection1d import NonlinearConvection1D
+from steps.diffusion1d import Diffusion1D
 
 # 1. Linear Convection 1D
 '''
 run_simulation(grid_points, grid_length, nt, dt, c)
-grid_points - number of grid points; nt - number of timesteps we want to calculate;
-dt - amount of time each timestep covers (delta t); c - wave speed
+grid_points - number of grid points; nt - number of time steps we want to calculate;
+dt - amount of time each tim estep covers (delta t); c - wave speed
 '''
 # LinearConvection1D().run_simulation()
 
 # 2. Nonlinear Convection 1D
-NonlinearConvection1D().run_simulation()
+'''
+run_simulation(grid_points, grid_length, nt, dt)
+grid_points - number of grid points; nt - number of time steps we want to calculate;
+dt - amount of time each time step covers (delta t)
+'''
+# NonlinearConvection1D().run_simulation()
+
+# 3. Diffusion Equation 1D
+# Diffusion1D().run_simulation()
+
+# 4. Burgers' Equation 1D
 
--- Navier-Stokes/steps/diffusion1d.py
+++ Navier-Stokes/steps/diffusion1d.py
@@ -1,10 +1,7 @@
 import numpy as np
 from matplotlib import pyplot as plt
-import time
-import sys
 
-
-class NonlinearConvection1D:
+class Diffusion1D:
     """
     Nonlinear Convection 1D
     """
@@ -12,29 +9,31 @@ class NonlinearConvection1D:
     iteration_number = 0
 
     def __init__(self):
-        print("Init Nonlinear Convection 1D")
+        print("Init Diffusion 1D")
 
-    def run_simulation(self, _grid_points=41, _grid_length=10, _nt=400, _dt=.025):
+    def run_simulation(self, _grid_points=61, _grid_length=4, _nt=400, _nu=0.3, _sig=0.2):
         grid_points = _grid_points  # number of grid points
         grid_length = _grid_length  # length of grid
         dx = grid_length / (grid_points - 1)  # distance between any pair of adjacent grid points
         nt = _nt  # nt is the number of time steps we want to calculate
-        dt = _dt  # dt is the amount of time each time step covers (delta t)
+        nu = _nu  # viscosity of the system
+        sig = _sig  # sigma - courant number
+        dt = sig * dx ** 2 / nu  # Dynamically scaling dt based on grid size to ensure convergence
 
         u = np.ones(grid_points)  # numpy function ones()
         u[int(.5 / dx):int(1 / dx + 1)] = 2  # setting u = 2 between 0.5 and 1 as per our I.C.s
 
-        self.draw_1d_plot(grid_length, grid_points, u, '1D Nonlinear Convection t=0')
+        self.draw_1d_plot(grid_length, grid_points, u, '1D Diffusion t=0')
 
-        un = np.ones(grid_points)
+        un = np.ones(grid_points) # placeholder array, un, to advance the solution in time
 
         for n in range(nt):  # Runs however many timesteps you set earlier
             un = u.copy()  # copy the u array to not overwrite values
-            for i in range(1, grid_points):
-                u[i] = un[i] - un[i] * dt / dx * (un[i] - un[i - 1])
+            for i in range(1, grid_points-1):
+                u[i] = un[i] + nu * (dt / dx**2) * (un[i+1] - 2*un[i] + un[i-1])
 
             if n % 20 == 0:
-                self.draw_1d_plot(grid_length, grid_points, u, '1D Nonlinear Convection t={}'.format(n/40))
+                self.draw_1d_plot(grid_length, grid_points, u, '1D Diffusion t={}'.format(n/40 + 0.5))
 
 
     def draw_1d_plot(self, grid_length, grid_points, u, text):
